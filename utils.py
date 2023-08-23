64d10a634dd25db428162c33f4885c7675520419
gil9red <ilya.petrash@inbox.ru>
2023-08-23 15:03:48 +0500

    Улучшение алгоритма определения имя хоста по IP

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ __author__ = "ipetrash"
 
 import re
 
-from subprocess import check_output
+from subprocess import check_output, STDOUT
 
 from flask import Request
 
@@ -20,7 +20,7 @@ def get_ip(rq: Request) -> str:
 
 
 def get_hostname(ip: str) -> str | None:
-    result: bytes = check_output(["nslookup", ip])
+    result: bytes = check_output(["nslookup", "-q=PTR", ip], stderr=STDOUT)
 
     m = re.search(rb"(\S+)\.employees\.", result)
     if not m:
